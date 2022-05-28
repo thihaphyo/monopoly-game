2a7c61d117315d8c9966ef75cd4c64cdd6d675d7
Phyothiha <phyothiha@codigo.sg>
2022-05-28 17:29:05 +0630

    merge: Logic + UI

---
+++ scripts/Objects.js
@@ -0,0 +1,228 @@
+
+const rule = {
+    "budgetLimit" : 0,
+    "property" : 1,
+    "mystery" : 2,
+    "tax":3,
+    "jail":4,
+    "free": 5,
+    "police":6
+}
+
+let tiles = [
+    new Tiles(
+        1,"Monopoly Pass", new TileType("budgetLimit", rule.budgetLimit), 200
+    ),
+    new Tiles(
+        2,"Tokyo", new TileType("property", rule.property), 200
+    ),
+    new Tiles(
+        3,"MysteryCard", new TileType("mystery", rule.mystery), null
+    ),
+    new Tiles(
+        4,"Oasaka", new TileType("property", rule.property), 200
+    ),
+    new Tiles(
+        5,"City Tax", new TileType("tax", rule.tax), 100
+    ),
+    new Tiles(
+        6,"Toll Gate", new TileType("tax", rule.tax), 100
+    ),
+    new Tiles(
+        7,"New York", new TileType("property", rule.property), 100
+    ),
+    new Tiles(
+        8,"MysteryCard", new TileType("mystery", rule.mystery), null
+    ),
+    new Tiles(
+        9,"San Fraciso", new TileType("property", rule.property), 100
+    ),
+    new Tiles(
+        10,"Washington", new TileType("property", rule.property), 100
+    ),
+    new Tiles(
+        11,"Police Station", new TileType("jail", rule.jail), null
+    ),
+    new Tiles(
+        12,"London", new TileType("property", rule.property), 140
+    ),
+    new Tiles(
+        13,"Museum", new TileType("property", rule.property), 140
+    ),
+    new Tiles(
+        14,"MysteryCard", new TileType("mystery", rule.mystery), null
+    ),
+    new Tiles(
+        15,"Tower", new TileType("property", rule.property), 140
+    ),
+    new Tiles(
+        16,"Toll Gate", new TileType("tax", rule.tax), 100
+    ),
+    new Tiles(
+        17,"Aqaurium", new TileType("property", rule.property), 180
+    ),
+    new Tiles(
+        18,"Ski Dubai", new TileType("property", rule.property), 180
+    ),
+    new Tiles(
+        19,"Rest", new TileType("free", rule.free), 0
+    ),
+    new Tiles(
+        20,"Marina Bay", new TileType("property", rule.property), 220
+    ),
+    new Tiles(
+        21,"MysteryCard", new TileType("mystery", rule.mystery), null
+    ),
+    new Tiles(
+        22,"Chinatown", new TileType("property", rule.property), 220
+    ),
+    new Tiles(
+        23,"Universal", new TileType("property", rule.property), 240
+    ),
+    new Tiles(
+        24,"Toll Gate", new TileType("tax", rule.tax), 100
+    ),
+    new Tiles(
+        25,"Jardin Du", new TileType("property", rule.property), 260
+    ),
+    new Tiles(
+        26,"Place dela", new TileType("property", rule.property), 260
+    ),
+    new Tiles(
+        29,"City Tax", new TileType("tax", rule.tax), 100
+    ),
+    new Tiles(
+        27,"Eiffle Tower", new TileType("property", rule.property), 280
+    ),
+    new Tiles(
+        28,"Police", new TileType("police", rule.police), 300
+    ),
+    new Tiles(
+        29,"Jeju Do", new TileType("property", rule.property), 300
+    ),
+    new Tiles(
+        30,"Seoul", new TileType("property", rule.property), 300
+    ),
+    new Tiles(
+        31,"Food Mall", new TileType("free", rule.free), 0
+    ),
+    new Tiles(
+        32,"Bulguka", new TileType("property", rule.property), 320
+    ),
+    new Tiles(
+        33,"Toll Gate", new TileType("tax", rule.tax), 100
+    ),
+    new Tiles(
+        34,"MysteryCard", new TileType("mystery", rule.mystery), null
+    ),
+    new Tiles(
+        35,"Pagodas", new TileType("property", rule.property), 350
+    ),
+    new Tiles(
+        36,"Mall", new TileType("property", rule.property), 400
+    ),
+    new Tiles(
+        37,"Monopoly Pass", new TileType("budgetLimit", rule.budgetLimit), 200
+    )
+];
+
+function Player(playeName, amount, ownedProperties, isInJail = false, currentPosition = 1) {
+    this.playeName = playeName;
+    this.amount = amount;
+    this.ownedProperties = ownedProperties;
+    this.isInJail = isInJail;
+    this.currentPosition = currentPosition;
+}
+
+Player.prototype.getLandingTile = function() {
+    let tile = tiles[this.currentPosition-1];
+    console.log(this.currentPosition-1);
+    return tile;
+}
+
+Player.prototype.landOnMysteryCard = function(onSuccess) {
+    let random = Math.floor(Math.random() * (100 - 20) + 20);
+    let plusOrMinus = Math.floor(Math.random() * (0 - 1) + 1);
+    if (plusOrMinus == 0) {
+        this.amount+=random;
+    } else {
+        this.amount-=random;
+    }
+    onSuccess(random, plusOrMinus);
+}
+
+Player.prototype.landOnTravelBudget = function(onSuccess) {
+    this.amount+=200;
+    onSuccess();
+}
+
+Player.prototype.collectTax = function(tax) {
+    this.amount+=tax;
+}
+
+Player.prototype.landedOnProperty = function(property,
+    onPromptUserToBuy,
+    onFailMoneyProcess,
+    onPayTax,onCollectRent) {
+    if(property.ownedBy == null) {
+        //can buy
+        if(this.amount >= property.tileAmount) {
+            console.log('asdsafscsfvsf');
+            onPromptUserToBuy(property);
+        } else {
+            onFailMoneyProcess();
+        }
+    } else if (property.ownedBy.playeName != this.playeName) {
+        //landed on other player's property Pay TAX!!!!
+        let tax = property.tileAmount * 0.1;
+        this.deduct(tax);
+        onPayTax(tax);
+    } else {
+        // landed on owned property collect rent
+        let rent = property.tileAmount * 0.08;
+        this.collectTax(rent);
+        onCollectRent(rent);
+        console.log('DO NOTHING');
+    }
+
+}
+
+Player.prototype.buyProperty = function(property, onSuccess) {
+    this.deduct(property.tileAmount);
+    tiles[tiles.indexOf(property)].addOwner(this); 
+    console.log(tiles);
+    this.ownedProperties.push(property.tileName);
+    onSuccess(property.tileAmount);
+} 
+
+Player.prototype.payGovTaxes = function(property, onSuccess) {
+    this.deduct(property.tileAmount);
+    onSuccess(property.tileAmount);
+} 
+
+Player.prototype.deduct = function(amt) {
+    this.amount-=amt;
+}
+
+function Tiles(tileID, tileName, tileType, tileAmount, ownedBy = null) {
+    this.tileID = tileID;
+    this.tileName = tileName;
+    this.tileType = tileType;
+    this.tileAmount = tileAmount;
+    this.ownedBy =  ownedBy;
+}
+
+Tiles.prototype.isOwnedByPlayer = function(player) {
+    return this.ownedBy.playeName == player.playeName;
+}
+
+Tiles.prototype.addOwner = function(player){
+    if (this.ownedBy == null) {
+        this.ownedBy = player;
+    }
+}
+
+function TileType(type, rule) {
+    this.type = type;
+    this.rule = rule;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,410 @@
-let playerCurrentPlace = {
-  0: 1,
-  1: 1,
-};
-let startPlaying = 0;
-let playerOne = true;
-const rollDice = () => {
-  let playerTurn = playerOne ? 0 : 1;
-  let currentPlace = playerCurrentPlace[playerTurn];
+let player1 = new Player("Logan", 1500, []);
+let player2 = new Player("Jame", 1500, []);
+let currentRoll = 0;
+let currentPlayer = player1;
+let log = [];
+let playersMoveOut = false;
+let isGameOver = false;
+let isPlayerDecided = false;
 
-  if (startPlaying != 2) {
-    document
-      .getElementById("B" + currentPlace)
-      .children[0].children[playerTurn].classList.add("hidden");
-    console.log(
-      document.getElementById("B" + currentPlace).children[0].children[
-        playerTurn
-      ]
+const readyPlayers = () => {
+  [player1, player2].forEach((v, i) => {
+    let index = i + 1;
+    if (v.playeName == currentPlayer.playeName) {
+      $("#turn").html(v.playeName + "'s turn");
+    }
+    $("#p" + index + "Name").html(v.playeName);
+    $("#p" + index + "Money").html("$" + v.amount);
+    $("#p" + index + "Property").html("");
+    v.ownedProperties.forEach((value) => {
+      $("#p" + index + "Property").append("<p>" + value + "</p>");
+    });
+  });
+  if (
+    [player1, player2].filter((data) => data.currentPosition != 0).length >= 2
+  ) {
+    playersMoveOut = true;
+  }
+  console.log(log);
+  // if (currentPlayer.playeName == player2.playeName) {
+  //     //ai
+  //     rollDice();
+  // }
+};
+
+const rollDice = () => {
+  let num1 = Math.floor(Math.random() * (6 - 1) + 1);
+  let num2 = Math.floor(Math.random() * (6 - 1) + 1);
+  currentRoll = num1 + num2;
+  $("#die1").attr("src", "assets/img/dice/dice_" + num1 + ".png");
+  $("#die2").attr("src", "assets/img/dice/dice_" + num2 + ".png");
+  document
+    .getElementById("diceContainer")
+    .setAttribute("class", "diceContainer");
+  $("#btnRollDice").prop("disabled", true);
+  $("#diceResult").html(currentPlayer.playeName + " got " + currentRoll);
+  movePlayer();
+};
+
+const movePlayer = () => {
+  // move & Game logic
+  let p = currentPlayer.playeName == player1.playeName ? 1 : 2;
+  if (!p.isInJail) {
+    for (let index = 0; index < currentRoll; index++) {
+      if (currentPlayer.currentPosition == 11) {
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("top")
+              .replace(/px/g, "")
+          ) + 190;
+        let pos2 =
+          Number(
+            $("#p" + p + "Icon")
+              .css("left")
+              .replace(/px/g, "")
+          ) - 20;
+        $("#p" + p + "Icon").css({ top: pos, left: pos2 });
+      } else if (
+        currentPlayer.currentPosition > 11 &&
+        currentPlayer.currentPosition < 19
+      ) {
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("top")
+              .replace(/px/g, "")
+          ) + 110;
+        $("#p" + p + "Icon").css({ top: pos });
+      } else if (currentPlayer.currentPosition == 19) {
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("left")
+              .replace(/px/g, "")
+          ) - 100;
+        $("#p" + p + "Icon").css({ left: pos });
+      } else if (
+        currentPlayer.currentPosition > 19 &&
+        currentPlayer.currentPosition < 29
+      ) {
+        if (currentPlayer.currentPosition == 28 && index == currentRoll - 1) {
+          console.log("Jailed");
+          let leftSide = 110 * 11 + 10;
+          let top = 10;
+          $("#p" + p + "Icon").css({ top: top, left: leftSide });
+          currentPlayer.currentPosition = 10;
+          if (currentPlayer.playeName == player1.playeName) {
+            currentPlayer.isInJail = true;
+            $("#p" + p + "Icon").css({ top: top, left: leftSide - 20 });
+          } else {
+            currentPlayer.isInJail = false;
+          }
+        } else {
+          let pos =
+            Number(
+              $("#p" + p + "Icon")
+                .css("left")
+                .replace(/px/g, "")
+            ) - 120;
+          $("#p" + p + "Icon").css({ left: pos });
+        }
+      } else if (currentPlayer.currentPosition == 29) {
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("top")
+              .replace(/px/g, "")
+          ) - 110;
+        $("#p" + p + "Icon").css({ top: pos });
+      } else if (
+        currentPlayer.currentPosition > 29 &&
+        currentPlayer.currentPosition < 37
+      ) {
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("top")
+              .replace(/px/g, "")
+          ) - 110;
+        $("#p" + p + "Icon").css({ top: pos });
+      } else if (currentPlayer.currentPosition == 37) {
+        currentPlayer.currentPosition = 0;
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("left")
+              .replace(/px/g, "")
+          ) + 110;
+        $("#p" + p + "Icon").css({ left: pos, top: "10px" });
+      } else {
+        let pos =
+          Number(
+            $("#p" + p + "Icon")
+              .css("left")
+              .replace(/px/g, "")
+          ) + 120;
+        $("#p" + p + "Icon").css({ left: pos });
+      }
+      console.log("ROll P" + currentPlayer.currentPosition);
+      currentPlayer.currentPosition++;
+    }
+
+    gameLogic();
+
+    log.push(
+      currentPlayer.playeName +
+        " rolled " +
+        currentRoll +
+        " and amt " +
+        currentPlayer.amount +
+        " and moved " +
+        currentPlayer.currentPosition
     );
-    startPlaying += 1;
+  } else {
+    log.push(
+      currentPlayer.playeName +
+        " rolled " +
+        currentRoll +
+        " and in jailed " +
+        currentPlayer.amount +
+        " and moved " +
+        currentPlayer.currentPosition
+    );
   }
 
-  let diceNumber = Math.floor(Math.random() * (7 - 1) + 1);
-  playerOne = playerOne ? false : true;
+  setTimeout(() => {
+    if (isGameOver) {
+      return;
+    }
+    // setInterval(() => {
+    //   if (isPlayerDecided) {
+    //     document
+    //       .getElementById("diceContainer")
+    //       .setAttribute("class", "diceContainer hidden");
+    //     if (currentPlayer.playeName == player1.playeName) {
+    //       p1.currentPosition = currentPlayer.currentPosition;
+    //       currentPlayer = player2;
+    //     } else {
+    //       p2.currentPosition = currentPlayer.currentPosition;
+    //       currentPlayer = player1;
+    //     }
+    //     currentRoll = 0;
+    //     $("#btnRollDice").prop("disabled", false);
+    //     readyPlayers();
+    //     isPlayerDecided = false;
+    //   }
+    // }, 1000);
+    document
+      .getElementById("diceContainer")
+      .setAttribute("class", "diceContainer hidden");
+    if (currentPlayer.playeName == player1.playeName) {
+      p1.currentPosition = currentPlayer.currentPosition;
+      currentPlayer = player2;
+    } else {
+      p2.currentPosition = currentPlayer.currentPosition;
+      currentPlayer = player1;
+    }
+    currentRoll = 0;
+    $("#btnRollDice").prop("disabled", false);
+    readyPlayers();
+    isPlayerDecided = false;
+  }, 1000);
 
-  //   playerCurrentPlace[playerTurn] += diceNumber;
-
-  //   let BoardNum = "B" + diceNumber;
-  //   let nextPlace = document.getElementById(BoardNum).id;
-
-  //   playerOne
-  //     ? loadHTML("playerAvatars.html", nextPlace)
-  //     : loadHTML("playerAvatars2.html", nextPlace);
-
-  //   playerOne = playerOne ? false : true;
-  //   console.log("turn" + playerTurn);
+  checkGameStatus();
 };
+
+$(document).ready(function () {
+  readyPlayers();
+});
+
+$("#btnRollDice").click(function () {
+  rollDice();
+});
+
+const gameLogic = () => {
+  if (isGameOver) {
+    return;
+  }
+
+  if (currentPlayer.playeName == player1.playeName) {
+    let tile = player1.getLandingTile();
+    if (tile.tileType.rule == rule.budgetLimit) {
+      if (playersMoveOut) {
+        player1.landOnTravelBudget(onSuccessPassTravelBudget);
+      }
+    } else if (tile.tileType.rule == rule.property) {
+      player1.landedOnProperty(
+        tile,
+        onPromptUserToBuy,
+        onFailMoneyProcess,
+        onPayTaxDuetoOtherPlayerProps,
+        onCollectRent
+      );
+    } else if (tile.tileType.rule == rule.tax) {
+      player1.payGovTaxes(tile, onPayGovTax);
+    } else if (tile.tileType.rule == rule.mystery) {
+      console.log("aaadsddsm");
+      player1.landOnMysteryCard(onSuccessMystery);
+    }
+  } else {
+    let tile = player2.getLandingTile();
+    if (tile.tileType.rule == rule.budgetLimit) {
+      if (playersMoveOut) {
+        player2.landOnTravelBudget(onSuccessPassTravelBudget);
+      }
+    } else if (tile.tileType.rule == rule.property) {
+      player2.landedOnProperty(
+        tile,
+        onPromptUserToBuy,
+        onFailMoneyProcess,
+        onPayTaxDuetoOtherPlayerProps,
+        onCollectRent
+      );
+    } else if (tile.tileType.rule == rule.tax) {
+      player2.payGovTaxes(tile, onPayGovTax);
+    } else if (tile.tileType.rule == rule.mystery) {
+      player2.landOnMysteryCard(onSuccessMystery);
+    }
+  }
+};
+
+function onCollectRent(rent) {
+  window.alert(
+    currentPlayer.playeName +
+      " collect rent of $" +
+      rent +
+      " from his owned property"
+  );
+}
+
+function onSuccessPassTravelBudget() {
+  window.alert(currentPlayer.playeName + " got $200");
+}
+
+function onPromptUserToBuy(tile) {
+  //ai
+  if (currentPlayer.playeName == player2.playeName) {
+    if (currentPlayer.amount > tile.tileAmount + 50) {
+      player2.buyProperty(tile, onSuccessBuy);
+    } else {
+      console.log("Player Skipped");
+    }
+  } else {
+    showPropertyBuyPrompt(tile, onConfirmBuyProperty, onSkipBuy);
+    // if (confirm("Buy " + tile.tileName + "?")) {
+    //     if(currentPlayer.playeName == player1.playeName) {
+    //         player1.buyProperty(tile, onSuccessBuy);
+    //     } else {
+    //         player2.buyProperty(tile, onSuccessBuy);
+    //     }
+    //   } else {
+    //       console.log('Player Skipped');
+    // }
+  }
+  //   isPlayerDecided = false;
+  //   showPropertyBuyPrompt(tile, onConfirmBuyProperty, onSkipBuy);
+}
+
+function onConfirmBuyProperty(prop) {
+  if (currentPlayer.playeName == player1.playeName) {
+    player1.buyProperty(prop, onSuccessBuy);
+  } else {
+    player2.buyProperty(prop, onSuccessBuy);
+  }
+  document
+    .getElementById("propertyBuyPrompt")
+    .setAttribute("class", "propertyBuy hidden");
+  // isPlayerDecided = true;
+}
+
+function onSkipBuy() {
+  console.log("Player Skipped");
+  document
+    .getElementById("propertyBuyPrompt")
+    .setAttribute("class", "propertyBuy hidden");
+  //isPlayerDecided = true;
+}
+
+function onFailMoneyProcess() {
+  window.alert(currentPlayer.playeName + " do not have enough money");
+  //isPlayerDecided = true;
+}
+
+function onPayGovTax(amt) {
+  window.alert(currentPlayer.playeName + " has to pay " + amt + " as Tax.");
+  //isPlayerDecided = true;
+}
+
+function onPayTaxDuetoOtherPlayerProps(tax) {
+  window.alert(
+    currentPlayer.playeName +
+      " paid " +
+      tax +
+      " for landing on other player's property"
+  );
+  if (currentPlayer.playeName != player1.playeName) {
+    player1.collectTax(tax);
+  } else {
+    player2.collectTax(tax);
+  }
+  //isPlayerDecided = true;
+}
+
+function onSuccessBuy(amt) {
+  window.alert(currentPlayer.playeName + " bought property that worth " + amt);
+  //isPlayerDecided = true;
+}
+
+function onSuccessMystery(random, plusOrMinus) {
+  window.alert(
+    currentPlayer.playeName +
+      " draws Mystery card and " +
+      (plusOrMinus == 0 ? "rewarded " : "taxed ") +
+      "$" +
+      random
+  );
+}
+
+function showPropertyBuyPrompt(property, onConfirm, onCancel) {
+  $("#amount").html("$" + property.tileAmount);
+  $("#property_name").html("$" + property.tileName);
+
+  $("#buy").click(function () {
+    console.log("here");
+    onConfirm(tile);
+  });
+
+  $("#cancel").click(function () {
+    onCancel();
+  });
+
+  document
+    .getElementById("propertyBuyPrompt")
+    .setAttribute("class", "propertyBuy");
+}
+
+const checkGameStatus = () => {
+  if (player1.amount <= 0) {
+    isGameOver = true;
+    window.alert(player2.playeName + " Wins");
+  } else if (player2.amount <= 0) {
+    isGameOver = true;
+    window.alert(player1.playeName + " Wins");
+  }
+
+  if (isGameOver) {
+    $("#btnRollDice").prop("disabled", true);
+  }
+};
+
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left,
+    top: rect.top,
+  };
+}
 
 function loadHTML(data, boardNum) {
   console.log("hello");
